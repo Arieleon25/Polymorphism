1f2926509d6185bc0195d84fbafd6eecdb135d27
Arieleon25 <Arieleon25@yahoo.com>
2023-03-16 21:48:07 -0400

    Add project files.

---
+++ Polymorphism/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class Employee : Person, IQuittabe
+    {
+        public override void SayName()
+        {
+            Console.WriteLine(firstName + " " + LastName);
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("This is my quit method");
+        }
+    }
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittabe quit = new Employee();
+            Employee employee = new Employee();
+            employee.firstName = "Sample";
+            employee.LastName = "Student";
+            employee.SayName();
+
+            quit.Quit();
+
+
+            Console.ReadLine();
+        }
+    }
+}
